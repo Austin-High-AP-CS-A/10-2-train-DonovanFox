5da6656ea31fb97f0f3deb59e509231239bdc6ab
Donovan Fox <you@example.com>
2024-01-31 09:38:16 -0600

    more railcars

--- 102-Train/Display.java
+++ 102-Train/Display.java
@@ -11,6 +11,9 @@ public class Display extends JPanel{
 		super.paintComponent(g); //satisfies the parent class' requirements.
 		setBackground(Color.WHITE);
 		RailCar numba1 = new RailCar(Color.black,0);
+		RailCar numba2 = new RailCar(Color.green,1);
+		RailCar numba3 = new RailCar(Color.red,2);
+		RailCar numba4 = new RailCar(Color.green,3);
 		numba1.drawCar(g);
 	}
 }
